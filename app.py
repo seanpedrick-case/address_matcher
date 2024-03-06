375f938857d1c4c53e4e4f32b99c53b33c73e634
Sean-Case <64902828+seanpedrick-case@users.noreply.github.com>
2024-03-06 16:24:14 +0000

    readme gradio sdk to 4.20.0. Removed port specification from app.py as was breaking it on HF spaces

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ with block:
 #block.queue().launch(debug=True) # root_path="/address-match", debug=True, server_name="0.0.0.0",
 
 # Simple run for AWS server
-block.queue().launch(server_port=7861) # root_path="/address-match", debug=True, server_name="0.0.0.0",
+block.queue().launch() # root_path="/address-match", debug=True, server_name="0.0.0.0", server_port=7861
 
 # Download OpenSSL from here: 
 # Running on local server with https: https://discuss.huggingface.co/t/how-to-run-gradio-with-0-0-0-0-and-https/38003 or https://dev.to/rajshirolkar/fastapi-over-https-for-development-on-windows-2p7d
